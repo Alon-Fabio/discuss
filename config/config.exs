141f5a03f6ad9cc4ab0b1b0d40b7988c34e45e66
Alon-Fabio <alon.the.fabio@gmail.com>
2020-06-29 14:34:00 +0300

    Ueberauth install & security

--- config/config.exs
+++ config/config.exs
@@ -5,6 +5,8 @@
 # is restricted to this project.
 use Mix.Config
 
+import_config "prod.secret.exs"
+
 # General application configuration
 config :discuss,
   ecto_repos: [Discuss.Repo]
@@ -25,3 +27,20 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+
+
+# config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+#   client_id: {:system, "client_ID"},
+#   client_secret: {:system, "secret"}
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: Application.get_env(:discuss, :oauth)[:github_client_id],
+  client_secret: Application.get_env(:discuss, :oauth)[:github_client_secret]
+
