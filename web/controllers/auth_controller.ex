66f18e0c301e2f98d70aa66b161e03f05667a855
Alon-Fabio <alon.the.fabio@gmail.com>
2020-06-30 12:23:53 +0300

    Oauth controller

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -1,12 +1,16 @@
 defmodule Discuss.AuthController do
     use Discuss.Web, :controller
     plug Ueberauth
+
+    alias Discuss.User
     
-    def callback(conn, parms) do
-        IO.puts("###########################################################################")
-        IO.inspect(conn.assigns)
-        IO.puts("###########################################################################")
-        IO.inspect(parms)
-        IO.puts("###########################################################################")
+    def callback(%{assigns: %{ueberauth_auth: auth}} = conn, parms) do
+        # IO.puts("################################################")
+        # IO.inspect(auth)
+        # IO.puts("################################################")
+       user_params = %{email: auth.info.email, token: auth.credentials.token, provider: "github"}
+
+       changeset = User.changeset{%User{}, user_params}
+        
     end
 end
